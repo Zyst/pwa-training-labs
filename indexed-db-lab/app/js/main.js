908f2546df1a5f21b994a670a4670259861e4c4c
Erick Romero <erick.romero@aeris.net>
2017-12-27 11:58:55 -0600

    Add Orders

--- indexed-db-lab/app/js/main.js
+++ indexed-db-lab/app/js/main.js
@@ -21,7 +21,7 @@ var idbApp = (function() {
     return
   }
 
-  var dbPromise = idb.open('couches-n-things', 4, upgradeDb => {
+  var dbPromise = idb.open('couches-n-things', 5, upgradeDb => {
     switch (upgradeDb.oldVersion) {
       case 0:
       // a placeholder case so that the switch block will
@@ -44,7 +44,10 @@ var idbApp = (function() {
         store.createIndex('description', 'description')
       }
 
-      // TODO 5.1 - create an 'orders' object store
+      case 4: {
+        console.log('Creating the orders object store')
+        upgradeDb.createObjectStore('orders', { keyPath: 'id' })
+      }
     }
   })
 
@@ -265,7 +268,54 @@ var idbApp = (function() {
   }
 
   function addOrders() {
-    // TODO 5.2 - add items to the 'orders' object store
+    dbPromise.then(db => {
+      var tx = db.transaction('orders', 'readwrite')
+      var store = tx.objectStore('orders')
+
+      var items = [
+        {
+          name: 'Cabinet',
+          id: 'ca-brn-ma',
+          price: 799.99,
+          color: 'brown',
+          material: 'mahogany',
+          description: 'An intricately-designed, antique cabinet',
+          quantity: 7
+        },
+        {
+          name: 'Armchair',
+          id: 'ac-gr-pin',
+          price: 299.99,
+          color: 'grey',
+          material: 'pine',
+          description: 'A plush recliner armchair',
+          quantity: 3
+        },
+        {
+          name: 'Couch',
+          id: 'cch-blk-ma',
+          price: 499.99,
+          color: 'black',
+          material: 'mahogany',
+          description: 'A very comfy couch',
+          quantity: 3
+        }
+      ]
+
+      return Promise.all(
+        items.map(item => {
+          console.log('Adding item: ', item)
+          return store.add(item)
+        })
+      )
+        .catch(e => {
+          tx.abort()
+          console.log(e)
+        })
+        .then(() => {
+          console.log('All items added successfully!')
+        })
+    })
   }
 
   function showOrders() {
